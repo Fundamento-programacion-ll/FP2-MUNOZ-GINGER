534d129f6da65ce55232816f4b9e0bdb17a81b5c
Ginger-a <60004141+Ginger-a@users.noreply.github.com>
2020-01-20 08:56:51 -0500

    creando

--- Mvs/src/vista/usuario.java
+++ Mvs/src/vista/usuario.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mvs;
+package vista;
 
 /**
  *
  * @author Sony Vaio
  */
-public class Mvs {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class usuario {
     
 }
