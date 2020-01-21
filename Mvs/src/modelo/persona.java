39c931a07175f2ae643243612a21e850d272380d
Ginger-a <60004141+Ginger-a@users.noreply.github.com>
2020-01-21 09:51:15 -0500

    mvc

--- Mvs/src/modelo/persona.java
+++ Mvs/src/modelo/persona.java
@@ -10,5 +10,95 @@ package modelo;
  * @author Sony Vaio
  */
 public class persona {
-    
+     String nombre ;
+     String apellidos;
+     int cedula;
+     int edad;
+     int telefono ;
+     String direccion;
+     char genero;
+     boolean estadocivil;
+
+    public persona(String nombre, String apellidos, int cedula, int edad, int telefono, String direccion, char genero, boolean estadocivil) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.genero = genero;
+        this.estadocivil = estadocivil;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public boolean isEstadocivil() {
+        return estadocivil;
+    }
+
+    public void setEstadocivil(boolean estadocivil) {
+        this.estadocivil = estadocivil;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre:"+ this.getNombre();
+    }
+     
+     
+             
 }
---
+++ Mvs/src/mvc/MVC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc;
+
+import modelo.persona;
+
+/**
+ *
+ * @author Sony Vaio
+ */
+public class MVC {
+    public static void main(String[]args){
+      persona g = new persona("Ginger", "Muñoz", 123456, 19, 0, "pintado", 'f', true);
+      persona a = new persona("Ginger", "Muñoz", 123456, 19, 0, "pintado", 'f', true);
+      persona s = new persona("Ginger", "Muñoz", 123456, 19, 0, "pintado", 'f', true);
+     
+    }
+}
